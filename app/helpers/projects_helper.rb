1056416252a5a906747eaa6e7d2e5fd9cbb61fde
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2019-10-22 18:33:47 -0300

    Fix tracks logged time and add total sums

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,6 @@
 module ProjectsHelper
+  def calculate_hours_from(tracks)
+    seconds_sum = tracks.reduce(0) { |seconds, track| seconds + distance_of_time_in_words_hash(track.from, track.to, :accumulate_on => :seconds)[:seconds] }
+    distance_of_time_in_words(Time.now, Time.now + seconds_sum, true, :accumulate_on => :hours)
+  end
 end

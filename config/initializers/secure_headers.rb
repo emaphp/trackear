130fae6ed2edcebb6fcde9f4b4e559fdcd392e2a
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2019-10-30 12:16:48 -0300

    Run rubocop

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 SecureHeaders::Configuration.default do |config|
-    config.x_frame_options = "DENY"
-    config.x_content_type_options = "nosniff"
-    config.x_xss_protection = "1; mode=block"
-    config.x_download_options = "noopen"
-    config.x_permitted_cross_domain_policies = "none"
-    config.referrer_policy = %w(origin-when-cross-origin strict-origin-when-cross-origin)
-    config.csp.default_src = %w('none')
-    config.csp.base_uri = %w('self')
-    config.csp.frame_ancestors = %w('none')
-    config.csp.form_action = %w('self')
-    config.csp.block_all_mixed_content = true
-    config.csp.upgrade_insecure_requests = true
+  config.x_frame_options = 'DENY'
+  config.x_content_type_options = 'nosniff'
+  config.x_xss_protection = '1; mode=block'
+  config.x_download_options = 'noopen'
+  config.x_permitted_cross_domain_policies = 'none'
+  config.referrer_policy = %w[origin-when-cross-origin strict-origin-when-cross-origin]
+  config.csp.default_src = %w[none]
+  config.csp.base_uri = %w[self]
+  config.csp.frame_ancestors = %w[none]
+  config.csp.form_action = %w[self]
+  config.csp.block_all_mixed_content = true
+  config.csp.upgrade_insecure_requests = true
 end

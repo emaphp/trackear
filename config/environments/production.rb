fc00aa01415b1486469fcd54275ae62ebdc96bd8
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2020-01-16 13:56:59 -0300

    Fix assets in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,10 +96,6 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_controller.asset_host = ENV['DOMAIN']
-
-  config.action_mailer.asset_host = ENV['DOMAIN']
-
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,

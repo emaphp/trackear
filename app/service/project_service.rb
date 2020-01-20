60bcd5919207de2b1f109ad9338359ebc975434e
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2020-01-20 17:55:51 -0300

    Fix clients from project in project service

--- app/service/project_service.rb
+++ app/service/project_service.rb
@@ -1,6 +1,6 @@
 class ProjectService
     def self.clients_from_project(project)
-        client_ids = project.project_contracts.where(activity: 'Owner').select(:id)
+        client_ids = project.project_contracts.where(activity: 'Owner').select(:user_id)
         User.where(id: client_ids)
     end
 end

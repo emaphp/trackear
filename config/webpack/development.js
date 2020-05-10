052dcdc418d4ea8c2d6f1bdb67fd9d586d447cc4
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2020-05-10 17:36:49 -0300

    Remove bundler analyzer from prod build

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+environment.plugins.append('BundleAnalyzer', new BundleAnalyzerPlugin({ analyzerMode: 'static' }))
+
 module.exports = environment.toWebpackConfig()

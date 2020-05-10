ededa248beae76c0f120401c62f595ec42df5b92
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2020-05-10 15:28:00 -0300

    Small fix in users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,16 @@
 
 class User < ApplicationRecord
   extend FriendlyId
+
   friendly_id :slug_candidates, use: :slugged
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
+         # :confirmable, :lockable,
          :trackable, :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :project_contracts
+  # has_many :project_invitations
   has_many :projects, through: :project_contracts
 
   has_many :activity_tracks
@@ -17,6 +20,10 @@ class User < ApplicationRecord
 
   has_many :expenses
   has_many :expense_invitations
+
+  def avatar_or_default
+    picture || ''
+  end
 
   def slug_candidates
     [%i[first_name last_name slug_suffix]]
@@ -44,6 +51,10 @@ class User < ApplicationRecord
 
   def currently_active_contract_for(project)
     project_contracts.currently_active.find_by(project: project)
+  end
+
+  def self.by_email_or_create(email)
+    User.where(email: email).first_or_create(password: Devise.friendly_token[0, 20])
   end
 
   def self.from_omniauth(access_token)

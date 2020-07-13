4698a2eaca0e6fcc3c939b9b102a1b78dd13789f
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2020-07-13 15:52:20 -0300

    Add dockerfile

--- db/migrate/20191010133108_create_project.rb
+++ db/migrate/20191010133108_create_project.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Project < ActiveRecord::Migration[5.2]
+class CreateProject < ActiveRecord::Migration[5.2]
   def change
     create_table :projects do |t|
       t.string :name

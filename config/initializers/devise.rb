e8cd193e83e3114f4575e7d15836cda4abe444a9
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2020-09-01 14:46:46 -0300

    Use env or credentials file to fetch google api info

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,7 +260,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :google_oauth2, Rails.application.credentials.google_client_id, Rails.application.credentials.google_client_secret, {
+  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'] || Rails.application.credentials.google_client_id, ENV['GOOGLE_CLIENT_SECRET'] || Rails.application.credentials.google_client_secret, {
     scope: 'userinfo.email, userinfo.profile',
     prompt: 'select_account'
   }

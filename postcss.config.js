fa15066657e26474cd8f5fd867b042fc4bf68d09
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2020-10-07 13:27:12 -0300

    Remove unused css in prod

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+let environment = {
   plugins: [
     require('autoprefixer'),
     require('postcss-import'),
@@ -12,3 +12,19 @@ module.exports = {
     })
   ]
 }
+
+if (process.env.RAILS_ENV === 'production') {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './app/**/*.html.erb',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/javascript/**/*.elm',
+      ],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  )
+}
+
+module.exports = environment;

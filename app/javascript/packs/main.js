59dae6c0c92e92b6cd467c7b880244daa3ef3a4e
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2020-10-07 16:01:16 -0300

    Fix tailwind and refactor menu

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,9 +1,13 @@
-import { Elm } from '../Main'
-import './main'
+// import { Elm } from '../Main'
+
+import 'cocoon-js'
+require('@rails/ujs').start()
+
+import "../css/main.css"
 
 document.addEventListener('DOMContentLoaded', () => {
   const target = document.createElement('div')
 
   document.body.appendChild(target)
-  Elm.Main.init({ node: target })
+  // Elm.Main.init({ node: target })
 })

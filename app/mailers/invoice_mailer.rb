868fb460ac3a95911cbd169ef111a265036a6228
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2020-10-17 12:50:18 -0300

    Add sengrid

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -1,14 +1,11 @@
 # frozen_string_literal: true
 
 class InvoiceMailer < ApplicationMailer
-  default from: 'BlackMountain <contact@black-mountain.com.ar>'
-
   def invoice_notify(invoice)
     @invoice = invoice
     mail(
-      to: 'BlackMountain <contact@black-mountain.com.ar>',
-      subject: 'BlackMountain - Invoice ' + @invoice.from.strftime('%B %Y'),
-      cc: ProjectService.clients_from_project(invoice.project).map(&:email)
+      to: '',
+      subject: 'Trackear - Invoice ' + @invoice.from.strftime('%B %Y')
     )
   end
 end
